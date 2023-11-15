71343eb730dc2a357da09095215707f13e006049
lantianqi <50139994+lantianqi@users.noreply.github.com>
2023-11-15 16:14:01 +1100

    update backend index

--- backend/index.js
+++ backend/index.js
@@ -55,6 +55,10 @@ app.get('/', async (req, res) => {
   })
 });
 
+app.get("/about", (req, res) => {
+  res.send("About route 🎉 ");
+});
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
